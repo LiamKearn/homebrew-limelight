d3b826a5a1ee351eb75bab25692adca717919fd6
lkearn <liam@sproutlabs.com.au>
2022-07-31 18:30:28 +1000

    Chore: Update formula to latest.

--- limelight.rb
+++ limelight.rb
@@ -1,8 +1,8 @@
 class Limelight < Formula
   desc "Standalone port of the yabai v2.4.3 border implementation"
   homepage "https://github.com/LiamKearn/limelight"
-  url "https://github.com/LiamKearn/limelight/archive/refs/tags/v1.0.tar.gz"
-  sha256 "57f8be0daf34d230f8c87b332e49b93308d63cd0d30795ba48198ecb5640eb31"
+  url "https://github.com/LiamKearn/limelight/archive/refs/tags/v1.1.tar.gz"
+  sha256 "496b79892ff891113b2c54738ea7e0fab67f75343a29aad08a805d151660d2c5"
   license "MIT"
 
   # This is what yabai depends on, It might be a bit out of date since limelight is now
